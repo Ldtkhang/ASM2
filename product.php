141214c39ab0650a6db27106e18d50da18b8aa51
TiKaay <107165428+Ldtkhang@users.noreply.github.com>
2022-11-12 01:15:40 +0700

    hii

--- product.php
+++ product.php
@@ -15,25 +15,103 @@
     </tr>
   </thead>
   <tbody>
-    <?php
-    $sql = "SELECT * FROM `product` a left join category b on a.catid = b.catid ";
-    $result = pg_query($conn, $sql);
-    while ($row = pg_fetch_array($result)) {
-    ?>
-      <tr>
-        <td scope="row"><?php echo $row['catid'] ?></td>
-        <td><?php echo $row['proid'] ?></td>
-        <td><?php echo $row['proname'] ?></td>
-        <td><?php echo $row['proprice'] ?></td>
-        <td><?php echo $row['proquantity'] ?></td>
-        <td> <img src="<?php echo '../admin/pimgs/' . $row['proimage']; ?>" style="width:150px" /></td>
-        <td><?php echo $row['prodescription'] ?></td>
+          <tr>
+        <td scope="row">1</td>
+        <td>1</td>
+        <td>Police Patrol Ship</td>
+        <td>48</td>
+        <td>10</td>
+        <td> <img src="../admin/pimgs/01.jpg" style="width:150px" /></td>
+        <td>OMGGG</td>
         <td>
-          <a href="?page=productupdate.php&id=<?php echo $row['proid'] ?>" class="glyphicon glyphicon-edit"></a> |
-          <a href="?page=productdelete.php&id=<?php echo $row['proid'] ?>" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+          <a href="?page=productupdate.php&id=1" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&id=1" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </tr>
+          <tr>
+        <td scope="row">2</td>
+        <td>2</td>
+        <td>ZW36 Rising Liger</td>
+        <td>70</td>
+        <td>10</td>
+        <td> <img src="../admin/pimgs/03.jpg" style="width:150px" /></td>
+        <td>WOW</td>
+        <td>
+          <a href="?page=productupdate.php&id=2" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&id=2" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </tr>
+          <tr>
+        <td scope="row">3</td>
+        <td>3</td>
+        <td>SD GUNDAM EX-STANDARD RX-93</td>
+        <td>55</td>
+        <td>10</td>
+        <td> <img src="../admin/pimgs/06.jpg" style="width:150px" /></td>
+        <td>Nice</td>
+        <td>
+          <a href="?page=productupdate.php&id=3" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&id=3" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </tr>
+          <tr>
+        <td scope="row">1</td>
+        <td>4</td>
+        <td>Cole's Fighter</td>
+        <td>35</td>
+        <td>10</td>
+        <td> <img src="../admin/pimgs/02.jpg" style="width:150px" /></td>
+        <td>Nice</td>
+        <td>
+          <a href="?page=productupdate.php&id=4" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&id=4" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </tr>
+          <tr>
+        <td scope="row">3</td>
+        <td>5</td>
+        <td>SD GUNDAM EX-STANDARD MSN-04</td>
+        <td>40</td>
+        <td>10</td>
+        <td> <img src="../admin/pimgs/05..jpg" style="width:150px" /></td>
+        <td>OMGGGGGG</td>
+        <td>
+          <a href="?page=productupdate.php&id=5" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&id=5" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </tr>
+          <tr>
+        <td scope="row">2</td>
+        <td>6</td>
+        <td>ZW22 Gilraptor </td>
+        <td>60</td>
+        <td>10</td>
+        <td> <img src="../admin/pimgs/04.jpg" style="width:150px" /></td>
+        <td>Good</td>
+        <td>
+          <a href="?page=productupdate.php&id=6" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&id=6" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </tr>
+          <tr>
+        <td scope="row">4</td>
+        <td>7</td>
+        <td>Legend Dr Strange</td>
+        <td>55</td>
+        <td>20</td>
+        <td> <img src="../admin/pimgs/07.jpg" style="width:150px" /></td>
+        <td>Very beautiful</td>
+        <td>
+          <a href="?page=productupdate.php&id=7" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&id=7" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </tr>
+          <tr>
+        <td scope="row">4</td>
+        <td>8</td>
+        <td>Legend Wong</td>
+        <td>50</td>
+        <td>20</td>
+        <td> <img src="../admin/pimgs/08.jpg" style="width:150px" /></td>
+        <td>So cool</td>
+        <td>
+          <a href="?page=productupdate.php&id=8" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&id=8" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
       </tr>
     <?php
-    }
     ?>
   </tbody>
 </table>
