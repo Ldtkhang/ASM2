c88ca9be0e3d48e78f2051aa76b0f831d28eebd2
TiKaay <107165428+Ldtkhang@users.noreply.github.com>
2022-11-12 00:50:53 +0700

    Update product.php

--- product.php
+++ product.php
@@ -1,6 +1,109 @@
 <h3>Product Management</h3>
 <hr style="background-color: blue; height:2px">
 <a class="btn btn-primary" href="?page=productadd.php">New Product</a>
+<div id="example_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer"><div class="row"><div class="col-sm-6"><div class="dataTables_length" id="example_length"><label>Show <select name="example_length" aria-controls="example" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-6"><div id="example_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="example"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="example" class="display dt-responsive nowrap dataTable no-footer dtr-inline collapsed" style="width: 100%; background-color: white;" aria-describedby="example_info">
+  <thead>
+    <tr><th class="sorting sorting_asc" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 79px;" aria-sort="ascending" aria-label="Category ID: activate to sort column descending">Category ID</th><th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 71px;" aria-label="Product ID: activate to sort column ascending">Product ID</th><th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 228px;" aria-label="Name: activate to sort column ascending">Name</th><th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 35px;" aria-label="Price: activate to sort column ascending">Price</th><th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 57px; display: none;" aria-label="Quantity: activate to sort column ascending">Quantity</th><th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 134px; display: none;" aria-label="Images: activate to sort column ascending">Images</th><th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 78px; display: none;" aria-label="Description: activate to sort column ascending">Description</th><th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 24px; display: none;" aria-label=": activate to sort column ascending"></th></tr>
+  </thead>
+  <tbody>
+          
+          
+          
+          
+          
+          
+          
+          
+      <tr class="odd">
+        <td scope="row" class="dtr-control sorting_1" tabindex="0">1</td>
+        <td>1</td>
+        <td>Police Patrol Ship</td>
+        <td>48</td>
+        <td style="display: none;">10</td>
+        <td style="display: none;"> <img src="../admin/pimgs/01.jpg" style="width:150px"></td>
+        <td style="display: none;">OMGGG</td>
+        <td style="display: none;">
+          <a href="?page=productupdate.php&amp;id=1" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&amp;id=1" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </td></tr><tr class="even">
+        <td scope="row" class="dtr-control sorting_1" tabindex="0">1</td>
+        <td>4</td>
+        <td>Cole's Fighter</td>
+        <td>35</td>
+        <td style="display: none;">10</td>
+        <td style="display: none;"> <img src="../admin/pimgs/02.jpg" style="width:150px"></td>
+        <td style="display: none;">Nice</td>
+        <td style="display: none;">
+          <a href="?page=productupdate.php&amp;id=4" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&amp;id=4" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </td></tr><tr class="odd">
+        <td scope="row" class="dtr-control sorting_1" tabindex="0">2</td>
+        <td>2</td>
+        <td>ZW36 Rising Liger</td>
+        <td>70</td>
+        <td style="display: none;">10</td>
+        <td style="display: none;"> <img src="../admin/pimgs/03.jpg" style="width:150px"></td>
+        <td style="display: none;">WOW</td>
+        <td style="display: none;">
+          <a href="?page=productupdate.php&amp;id=2" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&amp;id=2" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </td></tr><tr class="even">
+        <td scope="row" class="dtr-control sorting_1" tabindex="0">2</td>
+        <td>6</td>
+        <td>ZW22 Gilraptor </td>
+        <td>60</td>
+        <td style="display: none;">10</td>
+        <td style="display: none;"> <img src="../admin/pimgs/04.jpg" style="width:150px"></td>
+        <td style="display: none;">Good</td>
+        <td style="display: none;">
+          <a href="?page=productupdate.php&amp;id=6" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&amp;id=6" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </td></tr><tr class="odd">
+        <td scope="row" class="dtr-control sorting_1" tabindex="0">3</td>
+        <td>3</td>
+        <td>SD GUNDAM EX-STANDARD RX-93</td>
+        <td>55</td>
+        <td style="display: none;">10</td>
+        <td style="display: none;"> <img src="../admin/pimgs/06.jpg" style="width:150px"></td>
+        <td style="display: none;">Nice</td>
+        <td style="display: none;">
+          <a href="?page=productupdate.php&amp;id=3" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&amp;id=3" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </td></tr><tr class="even">
+        <td scope="row" class="dtr-control sorting_1" tabindex="0">3</td>
+        <td>5</td>
+        <td>SD GUNDAM EX-STANDARD MSN-04</td>
+        <td>40</td>
+        <td style="display: none;">10</td>
+        <td style="display: none;"> <img src="../admin/pimgs/05..jpg" style="width:150px"></td>
+        <td style="display: none;">OMGGGGGG</td>
+        <td style="display: none;">
+          <a href="?page=productupdate.php&amp;id=5" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&amp;id=5" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </td></tr><tr class="odd">
+        <td scope="row" class="dtr-control sorting_1" tabindex="0">4</td>
+        <td>7</td>
+        <td>Legend Dr Strange</td>
+        <td>55</td>
+        <td style="display: none;">20</td>
+        <td style="display: none;"> <img src="../admin/pimgs/07.jpg" style="width:150px"></td>
+        <td style="display: none;">Very beautiful</td>
+        <td style="display: none;">
+          <a href="?page=productupdate.php&amp;id=7" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&amp;id=7" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </td></tr><tr class="even">
+        <td scope="row" class="dtr-control sorting_1" tabindex="0">4</td>
+        <td>8</td>
+        <td>Legend Wong</td>
+        <td>50</td>
+        <td style="display: none;">20</td>
+        <td style="display: none;"> <img src="../admin/pimgs/08.jpg" style="width:150px"></td>
+        <td style="display: none;">So cool</td>
+        <td style="display: none;">
+          <a href="?page=productupdate.php&amp;id=8" class="glyphicon glyphicon-edit"></a> |
+          <a href="?page=productdelete.php&amp;id=8" onclick="return confirm('Are you sure?');" class="glyphicon glyphicon-remove"></a>
+      </td></tr></tbody>
+</table></div></div><div class="row"><div class="col-sm-5"><div class="dataTables_info" id="example_info" role="status" aria-live="polite">Showing 1 to 8 of 8 entries</div></div><div class="col-sm-7"><div class="dataTables_paginate paging_simple_numbers" id="example_paginate"><ul class="pagination"><li class="paginate_button previous disabled" id="example_previous"><a href="#" aria-controls="example" data-dt-idx="0" tabindex="0">Previous</a></li><li class="paginate_button active"><a href="#" aria-controls="example" data-dt-idx="1" tabindex="0">1</a></li><li class="paginate_button next disabled" id="example_next"><a href="#" aria-controls="example" data-dt-idx="2" tabindex="0">Next</a></li></ul></div></div></div></div>
 <table id="example" class="display dt-responsive nowrap" style="width:100%; background-color: white;">
   <thead>
     <tr>
