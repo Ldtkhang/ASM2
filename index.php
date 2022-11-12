5da7428b4431a789c5064147248b933f9143648e
TiKaay <107165428+Ldtkhang@users.noreply.github.com>
2022-11-12 19:58:44 +0700

    hihi

--- index.php
+++ index.php
@@ -15,5 +15,6 @@ $username = "tbemlhvelmhrao";
 $password = "af4d1bfb08947427e7ed2882faa904093c90e0d81bb7c55fb8ef346522f48d68";
 $db = "d1p5lbhl2umbl0";
 $conn = pg_connect('postgres://tbemlhvelmhrao:af4d1bfb08947427e7ed2882faa904093c90e0d81bb7c55fb8ef346522f48d68@ec2-52-200-215-149.compute-1.amazonaws.com:5432/d1p5lbhl2umbl0');
+ 
 
 include('theme.php');
