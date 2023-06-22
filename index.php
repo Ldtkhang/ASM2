ed508a032c133e2ea2d9d0d10867574ee10b9a29
Ldtkhang <tkth0306@gmail.com>
2023-06-22 15:36:14 +0700

    hi

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$urladmin ="https://khangtoy-7e1f5470f3ad.herokuapp.com/admin/";
-$urluser ="https://khangtoy-7e1f5470f3ad.herokuapp.com/";
+$urladmin ="https://khangtoy-2458c68992c0.herokuapp.com/admin/";
+$urluser ="https://khangtoy-2458c68992c0.herokuapp.com/";
 
 $home = "home.php";
 $register = "register.php";
